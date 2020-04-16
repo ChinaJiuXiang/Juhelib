85014195c9c1437c45a4e3f2ec1b5981ea483a55
Unnamed <952955808@qq.com>
2020-04-16 16:46:14 +0800

    update

--- common/functions.php
+++ common/functions.php
@@ -106,7 +106,6 @@ function httpPost($url, $data) {
     return $res;
 }
 
-
 /**
  * curl https get 方式访问
  * @param string $url 网址
@@ -149,4 +148,54 @@ function getSubstr($str, $leftStr, $rightStr) {
  */
 function object2Array($object) {
     return json_decode(json_encode($object, JSON_UNESCAPED_UNICODE), true);
+}
+
+/**
+ * 获取汉字拼音、单词的第一个字母
+ * @param $str
+ * @return string
+ */
+function getinitial($str)
+{
+    $asc = ord(substr($str, 0, 1));
+    if ($asc < 160) {
+        // 非中文
+        if ($asc >= 48 && $asc <= 57) {
+            return ''; // 数字
+        } elseif ($asc >= 65 && $asc <= 90) {
+            return chr($asc); // A--Z
+        } elseif ($asc >= 97 && $asc <= 122) {
+            return strtoupper(chr($asc - 32)); // a--z 强制转换为大写
+        } else {
+            return ''; // 其他
+        }
+    } else {
+        // 中文
+        $s = iconv("UTF-8", "gb2312", $str);
+        $asc = ord($s[0]) * 256 + ord($s[1]) - 65536;
+        if ($asc >= -20319 and $asc <= -20284) {return "A";}
+        if ($asc >= -20283 and $asc <= -19776) {return "B";}
+        if ($asc >= -19775 and $asc <= -19219) {return "C";}
+        if ($asc >= -19218 and $asc <= -18711) {return "D";}
+        if ($asc >= -18710 and $asc <= -18527) {return "E";}
+        if ($asc >= -18526 and $asc <= -18240) {return "F";}
+        if ($asc >= -18239 and $asc <= -17923) {return "G";}
+        if ($asc >= -17922 and $asc <= -17418) {return "H";}
+        if ($asc >= -17417 and $asc <= -16475) {return "J";}
+        if ($asc >= -16474 and $asc <= -16213) {return "K";}
+        if ($asc >= -16212 and $asc <= -15641) {return "L";}
+        if ($asc >= -15640 and $asc <= -15166) {return "M";}
+        if ($asc >= -15165 and $asc <= -14923) {return "N";}
+        if ($asc >= -14922 and $asc <= -14915) {return "O";}
+        if ($asc >= -14914 and $asc <= -14631) {return "P";}
+        if ($asc >= -14630 and $asc <= -14150) {return "Q";}
+        if ($asc >= -14149 and $asc <= -14091) {return "R";}
+        if ($asc >= -14090 and $asc <= -13319) {return "S";}
+        if ($asc >= -13318 and $asc <= -12839) {return "T";}
+        if ($asc >= -12838 and $asc <= -12557) {return "W";}
+        if ($asc >= -12556 and $asc <= -11848) {return "X";}
+        if ($asc >= -11847 and $asc <= -11056) {return "Y";}
+        if ($asc >= -11055 and $asc <= -10247) {return "Z";}
+        return '';
+    }
 }
