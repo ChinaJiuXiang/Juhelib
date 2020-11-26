59e175ca970f88d44c415300dc454f77dbddf06f
Unnamed <952955808@qq.com>
2020-11-26 09:21:32 +0800

    UPDATA CODE

--- src/ip.php
+++ src/ip.php
@@ -12,10 +12,11 @@ class ip
         $data = httpsGet("https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query=" . $ip . "&co=&resource_id=6006&t=1492224411362&ie=utf8&oe=gbk&cb=op_aladdin_callback&format=json&tn=baidu&cb=jQuery1102032941802880745374_1492224400641&_=1492224400643");
         $data = mb_convert_encoding($data, "utf-8", "gbk");
         $data = json_decode(getSubstr($data, "/**/jQuery1102032941802880745374_1492224400641(", ");"), true);
-        return [
-            "ip" => empty($data["data"][0]) ? $data["data"][0]["origip"] : "",
-            "address" => empty($data["data"][0]) ? $data["data"][0]["location"] : "",
-        ];
+        if(!empty($data["data"][0])) {
+            return ['ip' => $data['data'][0]['origip'], 'address' => $data['data'][0]['location']];
+        }else{
+            return self::qqwry($ip);
+        }
     }
 
     /**
@@ -25,9 +26,9 @@ class ip
      */
     protected static function taobao($ip)
     {
-        $data = json_decode(httpGet("http://ip.taobao.com/service/getIpInfo.php?ip=".$ip),true);
+        $data = json_decode(httpGet("http://ip.taobao.com/service/getIpInfo.php?accessKey=alibaba-inc&ip=".$ip),true);
         if($data["data"]["region"] != ""){
-            return ["ip" => $ip, "address" => $data["data"]["country"]."-".$data["data"]["area"]."地区 ".$data["data"]["region"].$data["data"]["city"]." ".$data["data"]["isp"]];
+            return ["ip" => $ip, "address" => $data["data"]["country"].$data["data"]["area"]." ".$data["data"]["region"].$data["data"]["city"]." ".$data["data"]["isp"]];
         }else{
             return ["ip" => $ip, "address" => $data["data"]["country"]];
         }
@@ -35,7 +36,7 @@ class ip
 
     /**
      * qqwry 地址库（本地）
-     * 版本：2020-07-30
+     * 版本：2020年11月25日
      * @param $ip
      * @return array
      */
