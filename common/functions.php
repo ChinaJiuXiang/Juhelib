f081c2a4fa79eb181dc7d7859107a2f9de952db1
Unnamed <952955808@qq.com>
2020-11-27 16:24:43 +0800

    UPDATE CODE

--- common/functions.php
+++ common/functions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 批量文本替换
+ * 批量文本替换（废弃：部分场景下不准确，建议使用 str_replace 函数）
  * @param string $context 源文本
  * @param array $keyword 需要替换的文本（数组）
  * @param array $keyword_replace 替换后的内容（数组）
@@ -8,7 +8,7 @@
  */
 function batch_str_replace($context, $keyword, $keyword_replace)
 {
-    if(sizeof($keyword) == 1 && sizeof($keyword_replace) == 1) {
+    /*if(sizeof($keyword) == 1 && sizeof($keyword_replace) == 1) {
         if(strpos($context, $keyword[0]) !== false) {
             $context = str_replace($keyword[0], $keyword_replace[0], $context);
         }else{
@@ -18,8 +18,8 @@ function batch_str_replace($context, $keyword, $keyword_replace)
         foreach ($keyword as $key => $value) {
             if(strpos($context, $keyword[$key]) !== false) { $context = str_replace($keyword[$key], $keyword_replace[$key], $context);}
         }
-    }
-    return $context;
+    }*/
+    return str_replace($keyword, $keyword_replace, $context);
 }
 
 /**
